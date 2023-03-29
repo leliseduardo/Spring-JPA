6dcf26d5fc6ec5463e19ba62a4f3994360a1cccc
Eduardo Lelis <eduardo.lelis.co@gmail.com>
2023-03-29 10:33:47 -0300

    Métodos HTTP - SB

---
+++ src/main/java/com/example/SpringJPA/controllers/MetodosHTTPController.java
@@ -0,0 +1,33 @@
+package com.example.SpringJPA.controllers;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/metodos")
+public class MetodosHTTPController {
+
+    @GetMapping
+    public String get() {
+        return "Requisição GET";
+    }
+
+    @PostMapping
+    public String post() {
+        return "Requisição POST";
+    }
+
+    @PutMapping
+    public String put() {
+        return "Requisição PUT";
+    }
+
+    @PatchMapping
+    public String patch() {
+        return "Requisição PATCH";
+    }
+
+    @DeleteMapping
+    public String delete() {
+        return "Requisição DELETE";
+    }
+}
