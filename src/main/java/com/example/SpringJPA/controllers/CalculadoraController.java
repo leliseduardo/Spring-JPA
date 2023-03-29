e1064cab7dfb41aa43e4872edb8345635876fd2d
Eduardo Lelis <eduardo.lelis.co@gmail.com>
2023-03-29 11:13:17 -0300

    Passando parâmetros para Web Service - SB

---
+++ src/main/java/com/example/SpringJPA/controllers/CalculadoraController.java
@@ -0,0 +1,6 @@
+package com.example.SpringJPA.controllers;
+
+public class CalculadoraController {
+
+    
+}
--- src/main/java/com/example/SpringJPA/controllers/ClienteController.java
+++ src/main/java/com/example/SpringJPA/controllers/ClienteController.java
@@ -1,18 +1,26 @@
 package com.example.SpringJPA.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.example.SpringJPA.models.Cliente;
 
 @RestController
-@RequestMapping(value = "/clientes", method = RequestMethod.GET)
+@RequestMapping("/clientes")
 public class ClienteController {
 
-    @GetMapping(path = "/qualquer")
+    @GetMapping( "/qualquer")
     public Cliente obterCliente(){
         return new Cliente(28, "Pedro", "123.456.789-00");
+    }
+
+    @GetMapping("/{id}")
+    public Cliente obterClientePorId1(@PathVariable int id){
+        return new Cliente(id, "Maria", "987.654.321-00");
+    }
+
+    @GetMapping
+    public Cliente obterClientePorId2(
+            @RequestParam(name = "id", defaultValue = "1") int id){
+        return new Cliente(id, "Joao Augusto", "111.222.333-44");
     }
 }
