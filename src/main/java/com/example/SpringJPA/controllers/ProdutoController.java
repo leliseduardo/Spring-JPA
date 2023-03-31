c2158bd0fedc4cbdb9d270e18051e273161df422
Eduardo Lelis <eduardo.lelis.co@gmail.com>
2023-03-31 13:49:54 -0300

    Consulta paginada - SB

--- src/main/java/com/example/SpringJPA/controllers/ProdutoController.java
+++ src/main/java/com/example/SpringJPA/controllers/ProdutoController.java
@@ -4,6 +4,8 @@ import com.example.SpringJPA.model.entities.Produto;
 import com.example.SpringJPA.model.repositories.ProdutoRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
@@ -30,7 +32,16 @@ public class ProdutoController {
         return produtoRepository.findAll();
     }
 
-    @GetMapping(value = "/{id}")
+    @GetMapping(path = "/pagina/{numeroPagina}/{qtdePagina}")
+    public Iterable<Produto> obterProdutosPorPagina(@PathVariable int numeroPagina, @PathVariable int qtdePagina){
+
+        if(qtdePagina >= 5) qtdePagina = 5;
+        Pageable page = PageRequest.of(numeroPagina, qtdePagina);
+
+        return produtoRepository.findAll(page);
+    }
+
+    @GetMapping(path = "/{id}")
     public Optional<Produto> obterProdutoPorId(@PathVariable int id){
 
         return produtoRepository.findById(id);
--- src/main/java/com/example/SpringJPA/model/repositories/ProdutoRepository.java
+++ src/main/java/com/example/SpringJPA/model/repositories/ProdutoRepository.java
@@ -2,7 +2,8 @@ package com.example.SpringJPA.model.repositories;
 
 import com.example.SpringJPA.model.entities.Produto;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface ProdutoRepository extends CrudRepository<Produto, Integer> {
+public interface ProdutoRepository extends CrudRepository<Produto, Integer>, PagingAndSortingRepository<Produto, Integer> {
 
 }
